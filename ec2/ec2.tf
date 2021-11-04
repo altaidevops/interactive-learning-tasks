d2d100a4fc03fac35ad5815d34d5e606e0d576c6
altaidevops <altai.devops@gmail.com>
2021-11-04 15:16:03 +0000

    updated.

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-    ami = "ami-ae6272b8"
+    ami = "ami-0447addf6c74624eb"
     instance_type = "t2.micro"
     availability_zone = "us-east-1a"
     user_data = file("userdata.sh")
