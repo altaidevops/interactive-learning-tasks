b317d5e696273c3e0a9d5174e069c004d43e64f8
“altaidevops” <altaidevops@gmail.com>
2021-11-13 20:33:25 +0000

    updated.

--- route53/route53.tf
+++ route53/route53.tf
@@ -3,10 +3,11 @@
 #   name = "altaidevops.com"
 # }
 
-resource "aws_route53_record" "blog" {
-  zone_id = "Z09740301PGDH4BXRF9CS"
-  name    = "blog.altaidevops.com"
-  type    = "A"
-  ttl     = "30"
-  records = ["127.0.0.1"]
-}
+# resource "aws_route53_record" "blog" {
+#   zone_id = "Z09740301PGDH4BXRF9CS"
+#   name    = "blog.altaidevops.com"
+#   type    = "A"
+#   ttl     = "30"
+#   records = ["127.0.0.1"]
+# }
+
